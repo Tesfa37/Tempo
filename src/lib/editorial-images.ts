70fa962126c7daea64926712db645d12ecc5fccc
Tesfatsion Desta <tesfatsion37@gmail.com>
2026-04-18 19:28:09 -0500

    feat(images): add editorial image path helpers

---
+++ src/lib/editorial-images.ts
@@ -0,0 +1,20 @@
+export const editorialImages = {
+  hero: '/images/editorial/hero-editorial.png',
+  categoryWomen: '/images/editorial/category-women.png',
+  categoryMen: '/images/editorial/category-men.png',
+  categoryAdaptive: '/images/editorial/category-adaptive.png',
+  featureMatchset: '/images/editorial/feature-matchset.png',
+  featureVirtualfit: '/images/editorial/feature-virtualfit.png',
+  insideSeatedFit: '/images/editorial/inside-seated-fit.png',
+  insideAdvisors: '/images/editorial/inside-advisors.png',
+  insidePassport: '/images/editorial/inside-passport.png',
+} as const
+
+export const productImages = {
+  'TMP-007': '/images/editorial/product-tmp-007.png',
+  'TMP-008': '/images/editorial/product-tmp-008.png',
+  'TMP-009': '/images/editorial/product-tmp-009.png',
+  'TMP-010': '/images/editorial/product-tmp-010.png',
+  'TMP-011': '/images/editorial/product-tmp-011.png',
+  'TMP-012': '/images/editorial/product-tmp-012.png',
+} as const
